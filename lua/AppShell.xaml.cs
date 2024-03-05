def74b43d713956be325e56c6fd1bcaaed808354
lua <leticiapires251106@gmail>
2024-03-05 09:39:43 -0300

    luaaaa

---
+++ lua/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace lua;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ lua/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace lua;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
